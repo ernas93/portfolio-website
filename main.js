4c812f8550aa304a534e5d5d44923855ed0b771e
ernas93 <107477572+ernas93@users.noreply.github.com>
2023-08-15 21:04:35 +0200

    update social links and add button for nala function

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 import './style.css';
+import nalaImg from './img/nala2.jpeg';
+
+(() => {
+  const nala = document.querySelector('#nala');
+
+  nala.addEventListener('click', () => {
+    const img = document.createElement('img');
+    img.src = nalaImg;
+    img.style.height = '100%';
+    img.style.position = 'fixed';
+    img.style.top = 0;
+    img.style.left = 0;
+
+    document.body.appendChild(img);
+
+    setTimeout(() => {
+      document.body.removeChild(img);
+    }, 2000);
+  });
+})();
